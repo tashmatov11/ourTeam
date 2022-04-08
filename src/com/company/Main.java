27c89589a7ab940a3769dfef5c90fe33817ae76e
Talgarbek00 <talgarbeknazarbekov00@gmail.com>
2022-04-08 18:32:10 +0600

    talgarbek

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Shattyk greets!");
         System.out.println("эмне жакпады");
+        System.out.println("talgarbek yryskul uulu ");
     }
 }
