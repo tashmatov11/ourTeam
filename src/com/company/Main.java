d788ef1310497021d434b89ed88f853c3452a865
OmurEgem <egember@gmail.com>
2022-04-08 18:22:04 +0600

    213212342

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Shattyk greets!");
         System.out.println("эмне жакпадe3213251ы");
+        //
     }
 }
