dd9cfdd93ff7a9380ddb411dcae6e7e2e266952d
OmurEgem <egember@gmail.com>
2022-04-08 18:30:03 +0600

    изменнения жома1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Shattyk greets!");
         System.out.println("эмне жакпадe3213251ы");
-        System.out.println("кандайсынар");
+        //
     }
 }
